0b39954dafb875c6fcb5abc37b94cebb7d2281c3
anjaa7 <76572251+anjaa7@users.noreply.github.com>
2023-02-19 20:15:22 +0100

    Create Insertion Sort.cpp

---
+++ Sorting Algorithms/Insertion Sort.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Selection sort algorithm O(n^2)
+// Repeatedly finds the minimum element in an unsorted portion of an array
+// Swaps it with the element at the beginning of that portion
+// Inefficient for large arrays, can be useful for sorting small arrays or arrays with large items
+
+void insertionSort (vector < int >&values)
+{
+  for (int i =; i < values.size (); i++)
+    {
+      int j = i - 1;
+      int tmp = values[i];
+      while (j >= 0 && values[j] > tmp)
+	{
+	  values[j + 1] = values[j];
+	  j--;
+	}
+      values[j + 1] = tmp;
+    }
+}
+
+int main ()
+{
+  vector<int>values;
+
+  values.push_back (9);
+  values.push_back (3);
+  values.push_back (7);
+  values.push_back (8);
+  values.push_back (2);
+  values.push_back (4);
+
+  insertionSort (values);
+
+  for (auto & it:values)
+  cout << it;
+
+  cout << endl;
+  return 0;
+}
